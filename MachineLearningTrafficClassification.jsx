d78485b0388d65064f1c8ba9853eb9c106812782
Zeid Zawaideh <zeidfz@csu.fullerton.edu>
2024-12-08 08:44:54 -0800

    Create MachineLearningTrafficClassification.jsx

---
+++ MachineLearningTrafficClassification.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const BuildWorld = () => {
+  return (
+    <div className="project-details">
+      <h1>Build Your Own World</h1>
+      <p>
+        Helped develop a Machine Learning Parser using LAF (Log Analysis Framework) to analyze and 
+        track network traffic for the 5G-Data subsystem. (1) Processes log entries extracting, computing, and storing 
+        results in table(s) for further analysis. (2) Parser detects the accuracy of flows for Gaming, OTT, and nRT regarding KPI 
+        latency performance. (3) Validated Gaming Automation Enhancements for multiple applications to ensure proper testing accuracy. 
+        (4) Scripted OTT Automation for Microsoft Teams Application: The Primary Device opens The application finds the desired contact 
+        and makes the call. The Secondary Device will answer, and the call will last for the desired time needed. 
+      </p>
+      <h2>Technologies Used:</h2>
+      <ul>
+        <li>Python</li>
+        <li>Key Performance Indicators</li>
+        <li>Data Structures</li>
+        <li>5G LTE</li>
+      </ul>
+      <h2>Project Outcome:</h2>
+      <p>
+        The high-level goal is to run the two automations while using the Parser to investigate the network traffic and accuracy of the KPI 
+        Latency performance for the application(s) in use.
+      </p>
+    </div>
+  );
+};
+
+export default Machine Learning Traffic Classification;
