e242d86058e3ba731618cf03ab0972a80f57b532
Zeid Zawaideh <zeidfz@gmail.com>
2025-09-20 14:36:36 -0700

    Update Projects.jsx

--- Projects.jsx
+++ Projects.jsx
@@ -4,24 +4,20 @@ import { motion } from 'framer-motion';
 const Projects = () => {
   const projects = [
     {
-      title: 'Machine Learning Traffic Classification',
-      description: 'High-level goal is running both OTT and Gaming Automations while using the Parser to investigate the netwrok traffic 
-        and accuracy of the KPI Latency performance for the application(s) in use.',
+      title: '',
+      description: '',
     },
     {
-      title: 'Satellite Over Snapdragon',
-      description: 'Using software development skills, investigated developing a mechanism incorporating end-to-end testing by looking into 
-      any delays and issues between the middleware and application layer. ',
-      link: 'https://www.forbes.com/sites/moorinsights/2023/01/11/snapdragon-satellite-and-the-state-of-satellite-mobile-communications/' 
+      title: '',
+      description: '',
     },
     {
-      title: 'Timeline Profiling API Library',
-      description: ' Refactored a timeline profiling API into a library, integrating it to extend log packet(s), enhancing tracking capabilities 
-      of sleep and wake-up activities for 5G LTE.',
+      title: '',
+      description: '',
     },
     {
       title: 'RaspberryPi Room Temprature Check',
-      description: 'In CPSC 440, I investigated a mechanism using RaspberryPi to detect the temprature of the room in both Farenheit/Celsius allowing 
+      description: 'In CPSC 440, I investigated a mechanism using RaspberryPi to detect the temprature of the room in both Farenheit & Celsius allowing 
       to check with a generated http link to place into your browser.',
     }
   ];
